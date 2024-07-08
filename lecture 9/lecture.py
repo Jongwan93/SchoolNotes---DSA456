6cfc460e9bb60db7662bdc7de2b4f71278591445
Jongwan93 <jwhur1993@gmail.com>
2024-07-08 14:25:04 -0400

    lecture 9 added

---
+++ lecture 9/lecture.py
@@ -0,0 +1,22 @@
+#heap
+#min-hip, max-hip
+
+# binary tree
+# - can have 0, 1, 2 nodes
+# - cannot have more than 2 nodes. if it does, it is NOT a binary tree
+# **Binary tree will be on final exam (1 question)**
+# - Ex) binary tree is given. is it completed binary tree? if not, redraw it.
+# - binary tree nodes must be inserted left to right order. cannot skip a node.
+
+# heap order property
+# - child must be greater than parent.
+# - parent of the node must have higher priority 
+# - child node must have lower priority
+# - min heap : it means all the parents are smaller than their child
+# - exam example: can you create min or max heap?
+# - ** try not to insert duplicated elements
+
+# another way to create min heap or max heap
+# "Heapify method"
+# - 1. create complete binary tree without worrying about sorting
+# - 2. and sort. (Swap)
